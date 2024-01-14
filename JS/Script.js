769b2c4f6db19fd7a1b889338f8e064d77f6ebd0
Malaikatshubuh07 <ahmadafiffarizki@gmail.com>
2024-01-15 00:20:31 +0700

    modifikasi 2

---
+++ JS/Script.js
@@ -0,0 +1,13 @@
+function hitung() {
+    document.getElementById('hasil').innerText = 'Hasil perhitungan';
+  }
+  
+  function riset() {
+    document.getElementById('inputNumber').value = '';
+    document.getElementById('hasil').innerText = '';
+  }
+  
+  function tampilkanLuas() {
+    document.getElementById('hasil').innerText = 'Luas persegi: ...';
+  }
+  
